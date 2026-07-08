193f8bc78d1dbf809e12fd8f5ff19d959c8c786f
Onyx_i7 <Crackdroid69@gmail.com>
2026-07-07 21:57:56 -0400

    Refactor RecipeRemover class to NoRecipeBook

--- src/main/java/com/onyxi7/reciperemover/RecipeRemover.java
+++ src/main/java/com/onyxi7/reciperemover/RecipeRemover.java
@@ -1,19 +1,19 @@
-package com.onyxi7.reciperemover;
+package com.onyxi7.norecipebook;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = RecipeRemover.MODID, name = RecipeRemover.NAME, version = RecipeRemover.VERSION, updateJSON = "https://raw.githubusercontent.com/Onyx-i7/RecipeRemover/refs/heads/main/modupdatechecker.json")
+@Mod(modid = NoRecipeBook.MODID, name = NoRecipeBook.NAME, version = NoRecipeBook.VERSION, updateJSON = "https://raw.githubusercontent.com/Onyx-i7/NoRecipeBook/refs/heads/main/modupdatechecker.json")
 public class RecipeRemover {
-    public static final String MODID = "reciperemover";
-    public static final String NAME = "Recipe Remover";
-    public static final String VERSION = "1.0.0";
+    public static final String MODID = "norecipebook";
+    public static final String NAME = "NoRecipeBook";
+    public static final String VERSION = "1.1.0";
     
     public static Logger logger;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        System.out.println("[RecipeRemover] Loaded correctly");
+        System.out.println("[NoRecipeBook] Loaded correctly");
     }
 }
